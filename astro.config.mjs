83feee6194c77b13efc85350809e9074570a0133
w1f666 <wyfinfinity@outlook.com>
2026-04-17 09:50:59 +0800

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL ?? 'https://example.com',
+	site: process.env.SITE_URL ?? 'https://w1f666.github.io',
+	base: '/infsblog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
